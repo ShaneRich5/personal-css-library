a0beff67642986919fdc04970477f5b392718c68
Shane Richards <shane.richards121@gmail.com>
2015-09-03 21:47:27 -0500

    configuring gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var csswring = require('csswring')
+var sass = require('gulp-sass');
+var autoprefixer = require('autoprefixer-core');
 
-gulp.task('default', function() {
-    console.log("works");
+gulp.task('styles', function() {
+    var processors = [
+        csswring,
+        autoprefixer({browsers:['last 2 version']})
+    ];
+
+    return gulp.src('styles.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('watch:styles', function() {
+    gulp.watch('**/*.scss', ['styles']);
 });
